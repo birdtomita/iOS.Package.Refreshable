9b408982a4ee74796ef3b4cb3a5429e0322aec58
birdtomita <bird.tomita@helplynet.com>
2023-09-15 17:12:14 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
 		.package(
 			name: "Introspect",
 			url: "https://github.com/siteline/SwiftUI-Introspect.git",
-			.exact: "1.0.0"
+			exact: "1.0.0"
 		)
     ],
     targets: [
